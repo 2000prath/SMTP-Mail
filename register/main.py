a8c33c131f024148ac49272dd969e01b1b65c8ed
2000prath <40172813+2000prath@users.noreply.github.com>
2020-02-01 14:39:08 +0530

    Add files via upload

---
+++ register/main.py
@@ -0,0 +1,47 @@
+from flask import Flask,request,render_template
+import smtplib 
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+app=Flask(__name__)
+
+students=[]
+
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+
+
+
+#register by emails
+@app.route("/register",methods=["POST"])
+def register():
+   name=request.form.get("name")
+   country=request.form.get("country")
+   email=request.form.get("email")
+   if not name :
+      return render_template("fails.html")
+   
+   students.append(name+" from "+country)
+   
+      
+  
+   try:
+      message="""Subject: this is subject """
+      server= smtplib.SMTP("smtp.gmail.com", 587)
+      server.starttls()
+      server.login("2000prathstorage@gmail.com","pythonpp@")
+      server.sendmail("2000prath1@gmail.com",email,message)
+   except:
+      return "something went wrong" 
+   return render_template("info.html",students=students)
+   
+
+
+
+
+   
+
+   
+   
+   
